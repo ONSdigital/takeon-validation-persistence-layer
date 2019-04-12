c4376b2443a1f66d3c1f197502aa2db9fb071044
RyanKilbane <ryankilbane92@hotmail.com>
2019-04-12 09:41:08 +0100

    added SCK dependencies, added helm chart in validationpersitencelayer

--- src/main/java/uk/gov/ons/collection/ValidationPersistenceLayer/ValidationPersistenceLayerApp.java
+++ src/main/java/uk/gov/ons/collection/ValidationPersistenceLayer/ValidationPersistenceLayerApp.java
@@ -3,11 +3,13 @@ package uk.gov.ons.collection.ValidationPersistenceLayer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RibbonClient("validationpersistencelayerapp")
 public class ValidationPersistenceLayerApp {
     public static void main(String[] args) {
         {
